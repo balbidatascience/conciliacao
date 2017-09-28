de33b7a7d58dd4f872b7c1b0baf7dba07f4d3277
LAPTOP\gabriel <balbidatascience@gmail.com>
2017-09-28 18:53:45 -0300

    subindo fonte

---
+++ Services/ConciliacaoRepository.py
@@ -0,0 +1,153 @@
+import pyodbc
+import  numpy as np
+import pandas as pd
+from datetime import datetime
+
+def openConn():
+    return pyodbc.connect('DRIVER={SQL Server};SERVER=.;DATABASE=IR;UID=sa;PWD=@gabriel');
+
+def openCursor(conn):
+    return conn.cursor();
+
+def execSelectTeste(cursor):
+    cursor.execute("select * from Teste");
+    x = [];
+    for row in cursor: x.append(print(row));
+    return x;
+
+def insertDsEstabelecimento(cursor, conn, dfHeader):
+    cursor.executemany(
+        "insert into dsEstabelecimentoAdquirente (Estabelecimento, VlrBruto, VlrComissao, VlrLiquido, VlrRejeitado) values (?,?,?,?,?)",
+        dfHeader.values.tolist());
+    conn.commit();
+    return True;
+
+def insertDsTransacaoAdquirente(cursor, conn, dfRows):
+    # Tranformando os valores NAN/NA por NONE (que é aceito como NULL no SQL SERVER)
+    dfRows = dfRows.astype(object).where(pd.notnull(dfRows), None);
+    #format datetime para mm/dd/yyyy
+    dfRows['Data'] = dfRows['Data'].map(lambda x: datetime.strptime(str(x), "%d/%m/%Y").strftime('%Y-%m-%d'), na_action='ignore')
+    dfRows['Dt. Captura'] = dfRows['Dt. Captura'].map(lambda x: datetime.strptime(str(x), "%d/%m/%Y").strftime('%Y-%m-%d'),na_action='ignore')
+    dfRows['Dt. Situação'] = dfRows['Dt. Situação'].map(lambda x: datetime.strptime(str(x), "%d/%m/%Y").strftime('%Y-%m-%d'),na_action='ignore')
+    #dfRows['Data'] = dfRows['Data'].map(lambda x:str(x).replace(" 00:00:00", ""))
+    #dfRows['Data'] = dfRows['Data'].map(lambda x:datetime.strptime(str(x), "%Y-%d-%m").strftime('%Y-%m-%d'), na_action='ignore')
+
+    cursor.executemany(
+        "INSERT INTO [dbo].[dsTransacaoAdquirente] ([NroAutorizacao] ,[IDERP] "
+        ",[IDEQUALS] ,[Adquirente] ,[Tipo] ,[Produto] ,[Estabelecimento] "
+        ",[CategoriaEstabelecimento] ,[Bandeira] ,[LoteRO] ,[NroTerminal] ,[MeioCaptura] ,"
+        "[Data] ,[DtCaptura] ,[Hora] ,[NroCartao] ,[OrigemCartao] ,[NSU] ,[TID] ,[Parc] ,"
+        "[LoteUnico] ,[NroReferencia] ,[NroFiliacao] ,[VlrBruto] ,[VlrComissao] ,[Taxa] ,"
+        "[TxContrato] ,[VlrLi­quido] ,[VlrRejeitado] ,[Situacao] ,[DtSituacao] ,[Conciliada] ,"
+        "[MJustificativa] ,[NomeTarefa] ,[SituacaoTarefa] ,[ConciliacaoParam]) "
+        "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+        dfRows.values.tolist());
+    conn.commit();
+    return True;
+
+
+def insertDsTransacaoIR(cursor, conn, dfRows):
+    # convert na, nan e nat para NULL
+    dfRows = dfRows.astype(object).where(pd.notnull(dfRows), None)
+
+    dfRows['Dt. Transação'] = dfRows['Dt. Transação'].map(lambda x: datetime.strptime(str(x), "%d/%m/%Y").strftime('%Y-%m-%d'), na_action='ignore')
+    dfRows['Dt. Venda'] = dfRows['Dt. Venda'].map(lambda x: datetime.strptime(str(x), "%d/%m/%Y").strftime('%Y-%m-%d'), na_action='ignore')
+
+    query = ("INSERT INTO [dbo].[dsTransacaoIR] "
+           "([NroAutorizacao] "
+           ",[IDERP] "
+           ",[Cliente] "
+           ",[Estabelecimento] "
+           ",[CategoriaEstabelecimento] "
+           ",[Adquirente] "
+           ",[idPagamento] "
+           ",[EstabERP] "
+           ",[CanalVenda] "
+           ",[NroCanal] "
+           ",[FormaPagto] "
+           ",[DtTransacao] "
+           ",[DtVenda] "
+           ",[LoteInterno] "
+           ",[LoteAdq] "
+           ",[IDContabilPagamento] "
+           ",[IDContabilVenda] "
+           ",[Bandeira] "
+           ",[NumCartaoCredito] "
+           ",[NSU] "
+           ",[TID] "
+           ",[Localizador] "
+           ",[MeioDeCaptura] "
+           ",[Parc] " 
+           ",[QtdPagamentos] "
+           ",[VlrPagamento] "
+           ",[VlrLiquido] "
+           ",[VlrParcela] "
+           ",[VlrAdic1] "
+           ",[VlrAdic2] "
+           ",[VlrDiferenca] "
+           ",[Resultado] "
+           ",[Conciliacao] "
+           ",[NomeTarefa] "
+           ",[SituacaoTarefa] "
+           ",[SituacaoParam] "
+           ",[SituacaoConcParam] "
+           ",[FormaPagParam]) "
+            "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+
+    cursor.executemany(query, dfRows.values.tolist())
+    conn.commit()
+
+    return True
+
+def InsertDsCancelamento(cursor, conn, dfRows):
+    # convert na, nan e nat para NULL
+    dfRows = dfRows.astype(object).where(pd.notnull(dfRows), None)
+
+    query = ("INSERT INTO [dbo].[dsCancelamento] "
+           "([TipoCancelamento] "
+           ",[NroAutorizacao] "
+           ",[IDERP] "
+           ",[Estabelecimento] "
+           ",[CategoriaEstabelecimento] "
+           ",[Adquirente] "
+           ",[Bandeira] "
+           ",[Tipo] "
+           ",[Produto] "
+           ",[Localizador] "
+           ",[NroTerminal] "
+           ",[LoteRO] "
+           ",[LoteUnico] "
+           ",[NrSequencia] "
+           ",[NrReferenciaPedido] "
+           ",[DtCaptura] "
+           ",[DtVenda] "
+           ",[DtCancelamento] "
+           ",[NroCartao] "
+           ",[NSU] "
+           ",[TID] "
+           ",[Parc] "
+           ",[VlrBruto] "
+           ",[VlrComissao] "
+           ",[VlrLiquido] "
+           ",[VlrBrutoCancelamento] "
+           ",[VlrLiquidoCancelamento] "
+           ",[MotivoCancelamento] "
+           ",[Conciliacao] "
+           ",[IDERPCanal] "
+           ",[NomeCanal] "
+           ",[NomeTarefa] "
+           ",[SituacaoTarefa]) "
+     "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
+
+    cursor.executemany(query, dfRows.values.tolist())
+    conn.commit()
+
+    return True
+
+#conn = openConn();
+#cursor = openCursor()
+
+#map(SqlService.insertDsEstabelecimento_tmp, cursor, conn,
+#    dfHeader['Nro. Autorização'], dfHeader['Vlr. Bruto'], dfHeader['Vlr. Comissão'], dfHeader['Vlr. Líquido'], dfHeader['Vlr. Rejeitado'])
+
+    #return print(insertDsEstabelecimento(cursor, conn));
